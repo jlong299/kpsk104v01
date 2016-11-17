487a3a700877a96c683ee89cdb54d18237410e25
jlong299 <jlong299@foxmail.com>
2016-11-17 10:53:54 +0800

    Add ce_LS.v ce_LS_scaling.v

---
+++ src/ce/ce_LS.v
@@ -0,0 +1,164 @@
+//-----------------------------------------------------------------
+// Module Name:        	ce_LS.v
+// Project:             CE RTL
+// Description:         Least Square before DCT
+// Author:				Long Jiang
+//------------------------------------------------------------------
+//  Version 0.1
+//  Description :  First version 
+//  2016-11-17
+//  ----------------------------------------------------------------
+//  Detail :  (Matlab Code)
+//
+//  H_freqUsed = rsInUsedSubcarrier_rx(:,iRxAnte) ./ rsInUsedSubcarrier_tx(:,iTxAnte,iUE);
+//
+//  1200 points
+//  --------------------------------------------------------------------------------------------------
+//  For ZC sequence, the modulus of dividend is constant (1). So division can be done by multiplication.
+//
+//  ---------------------------------------------------------------------------- 
+//  Note :  (1) fftpts_in : The number of FFT points is power of 2
+//          (2) Different UE has different LS dividend coefficients. 
+// 
+
+
+module ce_LS #(parameter  
+		wDataIn = 16,  
+		wDataOut =16  
+	)
+	(
+	// left side
+	input wire				rst_n_sync,  // clk synchronous reset active low
+	input wire				clk,    
+
+	input wire        sink_valid, // sink.sink_valid
+	output wire        sink_ready, //       .sink_ready
+	input wire [1:0]  sink_error, //       .sink_error
+	input wire        sink_sop,   //       .sink_sop
+	input wire        sink_eop,   //       .sink_eop
+	input wire [wDataIn-1:0] sink_real,  //       .sink_real
+	input wire [wDataIn-1:0] sink_imag,  //       .sink_imag
+
+	input wire [11:0] fftpts_in,    //       .fftpts_in
+
+	//right side
+	output wire        source_valid, // source.source_valid
+	input  wire        source_ready, //       .source_ready
+	output wire [1:0]  source_error, //       .source_error
+	output wire        source_sop,   //       .source_sop
+	output wire        source_eop,   //       .source_eop
+	output wire [wDataOut-1:0] source_real,  //       .source_real
+	output wire [wDataOut-1:0] source_imag,  //       .source_imag
+
+	output wire [11:0] fftpts_out    //       .fftpts_out
+	);
+
+localparam 	wCoeff = 18;
+
+reg        source_valid_t0; // source.source_valid
+reg        source_sop_t0;   //       .source_sop
+reg        source_eop_t0;   //       .source_eop
+reg [wDataIn+wCoeff+2-1:0] source_real_t0;  //       .source_real
+reg [wDataIn+wCoeff+2-1:0] source_imag_t0;  //       .source_imag
+
+wire [wCoeff-1:0] 	LS_RS_tx_real, LS_RS_tx_imag;
+
+assign fftpts_out = fftpts_in;
+assign source_error = 2'b00;
+assign sink_ready = source_ready;
+
+//-----------------------------------------------------
+//-----------  Part 1 :  division (multiplication)  ---
+//-----------------------------------------------------
+//  (sink_real + j*sink_imag) * (LS_RS_tx_real - j*LS_RS_tx_imag) / 63336
+
+// ---------------- Pipeline 1 -------------------------
+always@(posedge clk)
+begin
+	if (!rst_n_sync)
+	begin
+		p2[0] <= 0;
+		p2[1] <= 0;
+		p2[2] <= 0;
+		p2[3] <= 0;
+	end
+	else
+	begin
+		p1[0] <= sink_real * LS_RS_tx_real;
+		p1[1] <= sink_imag * LS_RS_tx_imag;
+		p1[2] <= sink_real * LS_RS_tx_imag;
+		p1[3] <= sink_imag * LS_RS_tx_real;
+	end
+end
+
+// ---------------- Pipeline 2 -------------------------
+always@(posedge clk)
+begin
+	if (!rst_n_sync)
+	begin
+		source_real_t0 <= 0;
+		source_imag_t0 <= 0;
+	end
+	else
+	begin
+		source_real_t0 <= p1[0]+p1[1];
+		source_imag_t0 <= -p1[2]+p1[3];
+	end
+end
+
+
+ce_LS_RS_tx #(  // Reference signal TX
+	            //   abs(source_real+j*source_imag) = 63336
+	.wDataOut (wCoeff)  
+	)
+ce_LS_RS_tx_inst (
+	// left side
+	.rst_n_sync 	(rst_n_sync),
+	.clk 			(clk),
+
+	.sink_valid 	(sink_valid),
+
+	.fftpts_in 		(fftpts_in),
+
+	// right side
+	// 1 clks delay with sink_valid
+	.source_real 	(LS_RS_tx_real ),  
+	.source_imag 	(LS_RS_tx_imag )
+	);
+
+//-----------------------------------------------------
+//-----------  Part 2 :  scaling -----------
+//-----------------------------------------------------
+ce_LS_scaling #(
+	.wDataIn (wDataIn+wCoeff+2),  
+	.wDataOut (wDataOut)  
+	)
+ce_LS_scaling_inst (
+	// left side
+	.rst_n_sync 	(rst_n_sync),
+	.clk 			(clk),
+
+	.sink_valid 	(source_valid_t0),
+	.sink_ready 	( ),
+	.sink_error 	(sink_error),
+	.sink_sop 		(source_sop_t0 ),    
+	.sink_eop 		(source_eop_t0 ),    
+	.sink_real 		(source_real_t0 ), 
+	.sink_imag 		(source_imag_t0 ), 
+
+	.fftpts_in 		(fftpts_in),
+
+	// right side
+	.source_valid 	(source_valid), 
+	.source_ready 	(source_ready), 
+	.source_error 	( ), 
+	.source_sop 	(source_sop ),   
+	.source_eop 	(source_eop ),   
+	.source_real 	(source_real ),  
+	.source_imag 	(source_imag ),  
+	.fftpts_out 	( )   
+
+	);
+
+
+endmodule
---
+++ src/ce/ce_LS_scaling.v
@@ -0,0 +1,96 @@
+//-----------------------------------------------------------------
+// Module Name:        	ce_LS_scaling.v
+// Project:             CE RTL
+// Description:         
+// Author:				Long Jiang
+//------------------------------------------------------------------
+//  Version 0.1
+//  Description :  First version 
+//  2016-11-17
+//  ----------------------------------------------------------------
+
+
+module ce_LS_scaling #(parameter  
+		wDataIn = 36,  
+		wDataOut =16  
+	)
+	(
+	// left side
+	input 					rst_n_sync,  // clk synchronous reset active low
+	input 					clk,    
+
+	input wire        	sink_valid, // sink.sink_valid
+	output reg        	sink_ready, //       .sink_ready
+	input wire [1:0]  	sink_error, //       .sink_error
+	input wire        	sink_sop,   //       .sink_sop
+	input wire        	sink_eop,   //       .sink_eop
+	input wire [wDataIn-1:0] sink_real,  //       .sink_real
+	input wire [wDataIn-1:0] sink_imag,  //       .sink_imag
+
+	input wire [11:0] fftpts_in,    //       .fftpts_in
+
+	//right side
+	output reg         source_valid, // source.source_valid
+	input  wire        	source_ready, //       .source_ready
+	output wire [1:0]  	source_error, //       .source_error
+	output reg        	source_sop,   //       .source_sop
+	output reg        	source_eop,   //       .source_eop
+	output reg [wDataOut-1:0] source_real,  //       .source_real
+	output reg [wDataOut-1:0] source_imag,  //       .source_imag
+	output wire [11:0] fftpts_out    //       .fftpts_out
+	);
+
+localparam 	divide_width = 16;    //   /256/256
+
+assign 	source_error = 2'b00;
+assign  fftpts_out = fftpts_in;
+assign  sink_ready = source_ready;
+
+always@(posedge clk)
+begin
+	if (!rst_n_sync)
+	begin
+		source_valid <= 0;
+		source_sop <= 0;
+		source_eop <= 0;
+	end
+	else
+	begin
+		source_valid <= sink_valid;
+		source_sop <= sink_sop;
+		source_eop <= sink_eop;
+	end
+end
+
+
+always@(posedge clk)
+begin
+	if (!rst_n_sync)
+	begin
+		source_real <= 0;
+		source_imag <= 0;
+	end
+	else
+	begin
+		begin
+			if ( sink_real[wDataIn-1:wDataOut+divide_width-1] == {(wDataIn - wDataOut -divide_width+1){1'b0}} ||
+				 sink_real[wDataIn-1:wDataOut+divide_width-1] == {(wDataIn - wDataOut -divide_width+1){1'b1}} )
+				source_real <= sink_real[wDataOut+divide_width-1:divide_width]+sink_real[divide_width-1]; //rounding (/65536)
+			else if ( sink_real[wDataIn-1] == 1'b0) // saturating
+				source_real <= { 1'b0, {(wDataOut-1){1'b1}} };
+			else
+				source_real <= { 1'b1, {(wDataOut-1){1'b0}} };
+
+			if ( sink_imag[wDataIn-1:wDataOut+divide_width-1] == {(wDataIn - wDataOut -divide_width+1){1'b0}} ||
+				 sink_imag[wDataIn-1:wDataOut+divide_width-1] == {(wDataIn - wDataOut -divide_width+1){1'b1}} )
+				source_imag <= sink_imag[wDataOut+divide_width-1:divide_width]+sink_imag[divide_width-1]; //rounding (/65536)
+			else if ( sink_imag[wDataIn-1] == 1'b0) // saturating
+				source_imag <= { 1'b0, {(wDataOut-1){1'b1}} };
+			else
+				source_imag <= { 1'b1, {(wDataOut-1){1'b0}} };
+		end
+		
+	end
+end
+
+endmodule
