72f21df30d5791d316b42bf8af7bf66876f5524c
jlong299 <jlong299@foxmail.com>
2016-11-18 11:55:59 +0800

    correct RStx mif file

--- matlab/coeff_CERS_gen.m
+++ matlab/coeff_CERS_gen.m
@@ -34,7 +34,7 @@ coeff_rs_tx_ro_imag = imag(coeff_rs_tx_ro);
 outf = fopen('../src/RAM_FIFO/coeff_RS_tx_UE0_real.mif','w');
 width = 18;
 depth = N;
-fprintf(outf,'WIDTH=%d;\nDEPTH=%d;\n\nADDRESS_RADIX=UNS;\nDATA_RADIX=HEX;\n\nCONTENT BEGIN\n',width,depth);
+fprintf(outf,'WIDTH=%d;\nDEPTH=%d;\n\nADDRESS_RADIX=UNS;\nDATA_RADIX=DEC;\n\nCONTENT BEGIN\n',width,depth);
 for k=1:N
     fprintf(outf,'%d:%d;\n',k-1, (coeff_rs_tx_ro_real(k) < 0)*(2^18) + coeff_rs_tx_ro_real(k)  );
 end
@@ -44,7 +44,7 @@ fclose(outf);
 outf = fopen('../src/RAM_FIFO/coeff_RS_tx_UE0_imag.mif','w');
 width = 18;
 depth = N;
-fprintf(outf,'WIDTH=%d;\nDEPTH=%d;\n\nADDRESS_RADIX=UNS;\nDATA_RADIX=HEX;\n\nCONTENT BEGIN\n',width,depth);
+fprintf(outf,'WIDTH=%d;\nDEPTH=%d;\n\nADDRESS_RADIX=UNS;\nDATA_RADIX=DEC;\n\nCONTENT BEGIN\n',width,depth);
 for k=1:N
     fprintf(outf,'%d:%d;\n',k-1, (coeff_rs_tx_ro_imag(k) < 0)*(2^18) + coeff_rs_tx_ro_imag(k)  );
 end
